9629f49ed761ad50ade119ade2b3c75441705952
jellygum <jellygum@naver.com>
2019-12-09 20:42:00 +0900

    [STM32F207] I2S Sample code 빌드.

--- Project/STM32F20x_StdPeriph_Examples/I2S/I2S_Audio/Src/system_stm32f2xx.c
+++ Project/STM32F20x_StdPeriph_Examples/I2S/I2S_Audio/Src/system_stm32f2xx.c
@@ -106,7 +106,8 @@
                variable is updated automatically.
   */
   uint32_t SystemCoreClock = 12000000;
-  __I uint8_t AHBPrescTable[16] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
+//  __I uint8_t AHBPrescTable[16] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
+  const uint8_t AHBPrescTable[16] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
 
 /**
   * @}
